9efaf0b30ad6b91ce9e34e270149baf7985e865e
Krasutski <Krasutski.Denis@gmail.com>
2016-09-06 23:04:27 +0300

    Update cx805.h

--- src/cx805.h
+++ src/cx805.h
@@ -35,7 +35,7 @@
 #define TIMER1_BASE_ADDR                    ((uint32_t)0x00041300)
 #define TIMER2_BASE_ADDR                    ((uint32_t)0x00041400)
 #define GENDEX_BASE_ADDR                    ((uint32_t)0x00041500)
-#define IR_TTRANS_BASE_ADDR                 ((uint32_t)0x00041600)
+#define IR_TRANS_BASE_ADDR                  ((uint32_t)0x00041600)
 #define INT_CONTROLLER2_BASE_ADDR           ((uint32_t)0x00041720)
 
 
@@ -55,7 +55,7 @@ typedef struct
     __IO uint16_t CS5_CONFIG;   /*!< EBI CS5 configuration register */
     __IO uint16_t CS6_CONFIG;   /*!< EBI CS6 configuration register */
     __IO uint16_t CS7_CONFIG;   /*!< EBI CS7 configuration register */
-}AMBA_TypeDef;
+}AMBA__TypeDef;
 
 typedef struct
 {
@@ -90,7 +90,7 @@ typedef struct
     __IO uint16_t StorageRegister13;    /*!< General Storage Register */
     __IO uint16_t StorageRegister14;    /*!< General Storage Register */
     __IO uint16_t StorageRegister15;    /*!< General Storage Register */    
-}RTC_TypeDef;
+}RTC__TypeDef;
 
 typedef struct 
 {
@@ -106,14 +106,230 @@ typedef struct
     __IO uint32_t INTRPTS;              /*!< Raw Interrupts Register */
     __IO uint16_t RESERVE;                
     __IO uint32_t ITYPE;                /*!< Interrupt Type Register */
-}IntController1_Typedef;
+}IntController1__TypeDef;
 
 typedef struct 
 {
     __IO uint16_t DATA;                 /*!< Writing inputs 8- or 16-bit data to the shift register. Reading reads 16-bit result */
     __IO uint16_t RST;                  /*!< Writing resets shift register to 0xFFFF */
-}CRC_Typedef;
+}CRC__TypeDef;
 
+typedef struct
+{
+    __IO uint16_t TX_CONTROL;
+    __IO uint16_t TX_BR;
+    __IO uint16_t TX_HOLDING;
+    __IO uint16_t TX_STATUS;
+    __IO uint16_t RX_CONTROL;
+    __IO uint16_t RX_BR;
+    __IO uint16_t RX_HOLDING;
+    __IO uint16_t RX_DELAY;
+    __IO uint8_t TIMEOUT;
+    __IO uint8_t RESERVED;              /*!< Requested Delay */
+    __IO uint16_t DELAY_COUNT;          /*!< Actual Delay Counted */
+}SerialPort_TypeDef;
+
+typedef struct 
+{
+    __IO uint16_t FRAME_CONTROL;
+    __IO uint16_t STATUS;
+    __IO uint16_t DATA;
+    __IO uint16_t NUMBER_OF_LAST_RXD_MSG;
+    __IO uint16_t NUMBER_OF_TX_MSG;
+    __IO uint16_t XON_AND_XOFF_CHARS;
+    __IO uint16_t NUMBER_OF_BOFS;
+    __IO uint16_t INT_CONTROL_STATUS1;
+    __IO uint16_t INT_CONTROL_STATUS2;
+    __IO uint16_t TEST_PPM;
+    __IO uint16_t CRC_RESULT_LS;
+    __IO uint16_t CRC_RESULT_MS;
+    __IO uint16_t TEST_FAULT;
+    __IO uint16_t BUFF_REG;
+    __IO uint16_t PPM_BIT_WIDTH;
+    __IO uint16_t TEST_SHIFT_REG;
+    __IO uint16_t TIMING_CONTROL;
+    __IO uint16_t BYTES_RXD;
+    __IO uint16_t BYTES_TXD;
+    __IO uint16_t BYTES_LAST_RXD;
+    __IO uint16_t CGARS_FOR_BOF_EOF;
+}IRDA_TypeDef;
+
+typedef struct 
+{
+    __IO uint16_t GRP10_CTL;                /*!< Groups 1 & 0 Drive and DC Controls */
+    __IO uint16_t GRP32_CTL;                /*!< Groups 3 & 2 Drive and DC Controls */
+    __IO uint16_t GRPA_CTL;                 /*!< Group A Oen and IE Controls */
+    __IO uint16_t GRPB_CTL;                 /*!< Group B Oen and IE Controls */
+    __IO uint16_t GRPC_CTL;                 /*!< Group C Oen and IE Controls */
+    __IO uint16_t GRPD_CTL;                 /*!< Group D Oen and IE Controls */
+    __IO uint16_t GRPA_DAT;                 /*!< Group A Data In & Data Out */
+    __IO uint16_t GRPB_DAT;                 /*!< Group B Data In & Data Out */
+    __IO uint16_t GRPC_DAT;                 /*!< Group C Data In & Data Out */
+    __IO uint16_t GRPD_DAT;                 /*!< Group D Data In & Data Out */
+    __IO uint16_t CHIP_ID1;                 /*!< Device Identification Registers */
+    __IO uint16_t CHIP_ID2;                 /*!< Device Identification Registers */
+    __IO uint16_t SPARE;                    /*!< Spare Register */
+    __IO uint16_t GRPE_CTL;                 /*!< Group E Control Register */
+    __IO uint16_t GRPE_DAT;                 /*!< Group E Data In & Data Out */
+    __IO uint16_t GRPE_DRV;                 /*!< Group E Drive Controls */
+    __IO uint16_t GRPE_DC;                  /*!< Group E Drive Controls */
+}GPIO_TypeDef;
+
+typedef struct
+{
+    __IO uint16_t DATA;
+    __IO uint16_t CLOCK;
+}I2C_TypeDef;
+
+typedef struct
+{
+    __IO uint16_t CONTROL;                  /*!< Sets mode of detector */
+    __IO uint16_t STATUS;                   /*!< Detector Status */
+    __IO uint16_t ESC_CHAR;                 /*!< Character to use as Escape Sequence Detection */
+    __IO uint16_t XON_CHAR;                 /*!< Character to detect as XON */
+    __IO uint16_t XOF_CHAR;                 /*!< Character to detect as XOFF */
+    __IO uint16_t ESC_TIMEOUT;              /*!< Number of 20ms periods to count */
+    __IO uint16_t DIVIDER;                  /*!< Prescaler divider */
+}ESD_TypeDef;
+
+typedef struct
+{
+    __IO uint16_t CONTROL;                  
+    __IO uint16_t BR;                       
+    __IO uint16_t CONFIG;                   
+}SDS_IRDA_PULSE_TypeDef;
+
+typedef struct
+{
+    __IO uint16_t CONTROL;
+    __IO uint16_t STATUS;
+    __IO uint16_t SOURCE_LS;
+    __IO uint16_t SOURCE_MS;
+    __IO uint16_t DECTINATION_LS;
+    __IO uint16_t DESTINATION_MS;
+    __IO uint16_t TRESHOLD_LS;
+    __IO uint16_t TRESHOLD_MS;
+    __IO uint16_t END_OF_BUFFER_LS;
+    __IO uint16_t END_OF_BUFFER_MS;
+    __IO uint16_t CURRENT_SOURCE_LS;
+    __IO uint16_t CURRENT_SOURCE_MS;
+    __IO uint16_t CURRENT_DESTINATION_LS;
+    __IO uint16_t CURRENT_DESTINATION_MS;    
+}DMA_Channel_TypeDef;
+
+typedef struct
+{
+    __IO uint16_t CONTROL;
+    __IO uint16_t CLOCK_COUNT;
+    __IO uint16_t COUNTER_LS;
+    __IO uint16_t COUNTER_MS;
+    __IO uint16_t SLEEP_COUNT_LS;
+    __IO uint16_t SLEEP_COUNT_MS;
+}SleepTmr_TypeDef;
+
+typedef struct
+{
+    __IO uint16_t DECISION[16];
+    __IO uint16_t CONTROL;
+    __IO uint16_t STATUS;
+    __IO uint16_t FAILURE;
+    __IO uint16_t CHARS;
+    __IO uint16_t BAUD_COUNTER;
+    __IO uint16_t SHIFT;
+    __IO uint16_t ASP_CONTROL;
+    __IO uint16_t BLOCK_STATE;
+}Autobaud_TypeDef;
+
+
+typedef struct
+{
+    __IO uint16_t CONTROL;                          /*<! GPIO for keypad control */
+    __IO uint16_t STROBE;                           /*<!  */
+    __IO uint16_t RETURN;                           /*<!  */
+}Keypad_TypeDef;
+
+
+typedef struct
+{
+    __IO uint16_t CONTROL1;
+    __IO uint16_t FORCE1;
+    __IO uint16_t CONTROL2;
+    __IO uint16_t FORCE2;
+    __IO uint16_t REQUEST_VECTOR;
+    __IO uint16_t DSP_CONFIG;
+    __IO uint16_t DSP_CONTROL;
+    __IO uint16_t DSP_INT;
+    __IO uint16_t ASIC_MODE_CONTROL;    
+}CCB_TypeDef;
+
+
+typedef struct
+{
+    __IO uint16_t CONTROL;
+    __IO uint16_t DUTY_CYCLE;
+    __IO uint16_t DIVIDER;
+    __IO uint16_t COUNT;
+    __IO uint16_t PATTERN_LS;
+    __IO uint16_t PATTERN_MS;
+}PWM_TypeDef;
+
+
+typedef struct
+{
+    __IO uint16_t CONTROL_LS;
+    __IO uint16_t CONTROL_MS;
+    __IO uint16_t STATUS;
+    __IO uint16_t INT_CONTROL;
+    __IO uint16_t OUT_BUFFER;
+    __IO uint16_t IN_BUFFER;
+    __IO uint16_t DMA_DELAY_COUNT;
+}SIM_TypeDef;
+
+
+typedef struct
+{
+    __IO uint16_t CONTROL;
+    __IO uint16_t DIVIDERS;
+    __IO uint16_t EXTERNAL_CONTROL;
+}PLL_TypeDef;
+
+
+typedef struct
+{
+    __IO uint16_t PERIOD_LS;                /*!< 16 bit */
+    __IO uint16_t PERIOD_MS;                /*!< 10 bit */
+    __IO uint16_t PERIOD_HOLDING_LS;        /*!< 16 bit */
+    __IO uint16_t PERIOD_HOLDING_MS;        /*!< 10 bit */
+    __IO uint16_t COUNT_LS;                 /*!< Count value 16 bit */
+    __IO uint16_t COUNT_MS;                 /*!< Count value 10 bit */
+    __IO uint16_t COMTROL                   /*!< Timer Control Register */
+}Timer_TypeDef;
+
+
+typedef struct
+{
+    __IO uint16_t CONTROL;
+    __IO uint16_t STATUS;
+    __IO uint16_t KEY_1;
+    __IO uint16_t KEY_2;
+    __IO uint16_t KEY_3;
+    __IO uint16_t KEY_4;
+    __IO uint16_t IN_1;
+    __IO uint16_t IN_2;
+    __IO uint16_t FRAME_LENGHT;
+    __IO uint16_t NCRC;
+    __IO uint16_t OFFSET;
+    __IO uint16_t CRC_LS;
+    __IO uint16_t CRC_MS;
+}GENDEX_TypeDef;
+
+
+typedef struct
+{
+    __IO uint16_t CONTROL;
+    __IO uint16_t BAUD_GEN;
+    __IO uint16_t CONF;
+}IR_TRANS_TypeDef;
 
 
 
