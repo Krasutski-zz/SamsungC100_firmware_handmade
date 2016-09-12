afc6e483fadd776921294a3a804305bb06894f5f
denis_krasutski <denis_krasutski@cedoni.com>
2016-09-12 21:22:28 +0300

    Add I2C files (empty)

---
+++ src/Drivers/cx805_hal/inc/cx805_i2c.h
@@ -0,0 +1,35 @@
+
+/* begin: cx805_i2c.h ----- */
+/* created 9/12/2016 by user ----------------------------------- */
+
+
+#ifndef __CX805_I2C_H__
+#define __CX805_I2C_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+/* ----- Includes ----------------------------------------------------------- */
+#include <cx805_hal.h>
+
+/* ----- Types -------------------------------------------------------------- */
+
+
+/* ----- Settings ----------------------------------------------------------- */
+
+
+/* ----- Prototypes --------------------------------------------------------- */
+HAL_StatusTypeDef HAL_I2C_Init();
+HAL_StatusTypeDef HAL_I2C_Write(uint8_t Addr, uint8_t *Data, uint8_t Lenght);
+HAL_StatusTypeDef HAL_I2C_Read(uint8_t Addr, uint8_t *Data, uint8_t Lenght);
+
+
+#ifdef __cplusplus
+ }
+#endif /* __cplusplus */
+
+
+#endif /* __CX805_I2C_H__ */
+
+/* end: cx805_i2c.h ----- */
